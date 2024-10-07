03d1f359574d59f8ec81a9d204afec3f49d982e9
ciel0217 <karimu0217@icloud.com>
2024-10-07 23:01:18 +0900

    Init

---
+++ texture_atlas/texture_atlas.py
@@ -0,0 +1,17 @@
+from PIL import Image
+
+# 画像を読み込む
+image1 = Image.open("1.jpeg")
+image2 = Image.open("2.jpeg")
+
+# アトラス用の大きなキャンバスを作成
+atlas_width = image1.width + image2.width
+atlas_height = image1.height + image2.height
+atlas = Image.new('RGBA', (atlas_width, atlas_height))
+
+# 画像を配置
+atlas.paste(image1, (0, 0))
+atlas.paste(image2, (0, image1.height))
+
+# アトラスを保存
+atlas.save("atlas.png")
